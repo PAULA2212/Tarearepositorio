5570c8c88300cadb56d0a2f57bbf2d338d8124f7
34656 <paulibfg@gmail.com>
2023-05-12 20:19:53 +0200

    v2

--- repositorio.py
+++ repositorio.py
@@ -1,6 +1,8 @@
-from ejerciciov1 import sum
+from ejerciciov1 import sum, res
 operacion=("suma"| "resta" | "multiplicacion")
 def calcular_resultado(n1,n2):
     if operacion=="suma":
         resultado = sum(n1,n2)
+    elif operacion=="resta":
+        resultado = res(n1,n2)
     return f'el resultado de esta operacion es {resultado}'
